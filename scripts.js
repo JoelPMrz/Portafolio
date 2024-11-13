8fce0f1eb2f5748c50cbf5be1e121ba3151a7a73
JoelPMrz <joelperezmartinez01@gmail.com>
2024-11-13 16:51:51 +0100

    version 2.0

--- scripts.js
+++ scripts.js
@@ -59,58 +59,51 @@ function cambiarProyecto(proyecto) {
     const descripcionElemento = descripcionProyecto.querySelector("p");
     const videoProyecto = document.getElementById("video-proyecto");
     const linkGithub = document.getElementById("link-github");
-
-    // Obtener todas las miniaturas de los proyectos y eliminar la clase "activo"
     const miniaturas = document.querySelectorAll(".miniatura-proyecto");
     miniaturas.forEach(miniatura => miniatura.classList.remove("activo"));
     
-    // Aplicar la clase "activo" a la miniatura seleccionada
     const miniaturaSeleccionada = Array.from(miniaturas).find(miniatura => miniatura.getAttribute("onclick").includes(proyecto));
     if (miniaturaSeleccionada) {
         miniaturaSeleccionada.classList.add("activo");
     }
 
-    // Transición de salida de título, descripción y video
     tituloElemento.style.opacity = '0';
     tituloElemento.style.transform = 'translateY(10px)';
     descripcionElemento.style.opacity = '0';
     descripcionElemento.style.transform = 'translateY(10px)';
     videoProyecto.style.opacity = '0';
 
-    // Cambiar el contenido después de la transición de salida
     setTimeout(() => {
-        // Actualizar título, descripción y enlace de GitHub
         tituloElemento.textContent = proyectos[proyecto].titulo;
         descripcionElemento.innerHTML = proyectos[proyecto].descripcion.replace(/\n/g, '<br>');
         linkGithub.href = proyectos[proyecto].github;
 
-        // Mostrar u ocultar el video según si está definido
         if (proyectos[proyecto].video) {
             videoProyecto.src = proyectos[proyecto].video;
-            videoProyecto.style.display = "block"; // Mostrar video si está definido
-            videoProyecto.load(); // Cargar el nuevo video
-            videoProyecto.play(); // Reproducir el video
+            videoProyecto.style.display = "block"; 
+            videoProyecto.load(); 
+            videoProyecto.play(); 
         } else {
-            videoProyecto.style.display = "none"; // Ocultar video si no está definido
+            videoProyecto.style.display = "none"; 
         }
 
-        // Mostrar u ocultar el logo de tecnología según si está definido
+
         if (proyectos[proyecto].tecnologia) {
             logoTecnologia.src = proyectos[proyecto].tecnologia;
-            logoTecnologia.style.display = "block"; // Mostrar logo si está definido
+            logoTecnologia.style.display = "block";
         } else {
-            logoTecnologia.style.display = "none"; // Ocultar logo si no está definido
+            logoTecnologia.style.display = "none";
         }
 
-        // Transición de entrada de título y descripción
+      
         setTimeout(() => {
             tituloElemento.style.opacity = '1';
             tituloElemento.style.transform = 'translateY(0)';
             descripcionElemento.style.opacity = '1';
             descripcionElemento.style.transform = 'translateY(0)';
-            videoProyecto.style.opacity = '1'; // Hacer visible el video si se muestra
+            videoProyecto.style.opacity = '1';
         }, 50);
-    }, 300); // Duración de la transición de salida
+    }, 300); 
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -125,17 +118,17 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function () {
     const h1 = document.getElementById("bienvenida-h1");
     const p = document.getElementById("bienvenida-p");
-    const text = h1.innerText.replace(/ /g, "\u00A0"); // Reemplaza los espacios por espacios no rompibles
-    h1.innerText = ""; // Vacía el contenido inicial para comenzar el efecto
+    const text = h1.innerText.replace(/ /g, "\u00A0");
+    h1.innerText = ""; 
 
     let i = 0;
     function typeWriter() {
         if (i < text.length) {
             h1.innerText += text.charAt(i);
             i++;
-            setTimeout(typeWriter, 100); // Ajusta la velocidad de tipeo aquí
+            setTimeout(typeWriter, 100); 
         } else {
-            p.style.opacity = "1"; // Muestra el párrafo una vez completado el título
+            p.style.opacity = "1"; 
         }
     }
 
